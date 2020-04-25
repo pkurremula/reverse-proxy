f4196f94b75a103364db6c106268bff292281245
Torgeir Helgevold <torgeirh@hotmail.com>
2020-04-25 12:05:32 -0400

    wire up sample

--- samples/ReverseProxy.Sample/Controllers/HealthController.cs
+++ samples/ReverseProxy.Sample/Controllers/HealthController.cs
@@ -5,22 +5,19 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.ReverseProxy.Utilities;
 
-namespace Microsoft.ReverseProxy.Sample.Controllers
-{
+namespace Microsoft.ReverseProxy.Sample.Controllers {
     /// <summary>
     /// Controller for health check api.
     /// </summary>
     [ApiController]
-    public class HealthController : ControllerBase
-    {
+    public class HealthController : ControllerBase {
         private readonly ILogger<HealthController> _logger;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="HealthController"/> class.
         /// </summary>
-        public HealthController(ILogger<HealthController> logger)
-        {
-            Contracts.CheckValue(logger, nameof(logger));
+        public HealthController (ILogger<HealthController> logger) {
+            Contracts.CheckValue (logger, nameof (logger));
             _logger = logger;
         }
 
@@ -28,11 +25,10 @@ namespace Microsoft.ReverseProxy.Sample.Controllers
         /// Returns 200 if Proxy is healthy.
         /// </summary>
         [HttpGet]
-        [Route("/api/health")]
-        public IActionResult CheckHealth()
-        {
+        [Route ("/api/health")]
+        public IActionResult CheckHealth () {
             // TODO: Implement health controller, use guid in route.
-            return Ok();
+            return Ok ("OK");
         }
     }
 }
